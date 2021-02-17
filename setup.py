2914d74b2c1b8ea0d3a7718936aedbe20c6f8067
aditya <aditya.das2797@gmail.com>
2021-02-17 13:56:01 -0500

    checking packaging. added settings support.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
         name = 'todo',
-        version = '0.1.0',
-        packages = ['todo']
+        version = '0.0.1',
+        packages = ['todo'],
         entry_points = {
             'console_scripts': [
                 'todo = todo.__main__:main'
--- todo/__main__.py
+++ todo/__main__.py
@@ -1,31 +1,43 @@
 import argparse 
 import os
+import sys
 import json
 from .todo import todoList
 
-def main(args):
-    if not os.path.isfile('./settings.json'):
-        print("settings.json file not found. Please set it using --settings flag")
-        #exit
-    else:
-        with open('./settings.json', 'r') as settingsFile:
-            settings = json.load(settingsFile)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--edit")
+    parser.add_argument("--remove")
+    parser.add_argument("--settings")
+    args = parser.parse_args()
 
-    dirpath = settings["save_loc"]
+
+    settingsPath = os.path.join(os.path.split(sys.executable)[0], 
+                            'settings.json')
     if args.settings:
         settings = {}
-        settings["save_loc"] = str(input("Save path:"))
-        with open("./settings.json", "w") as settingsFile:
+        saveLoc = str(input("Save path for lists:"))
+        settings["save_loc"] = os.path.join(saveLoc,'saved_lists')
+        os.makedirs(settings["save_loc"], exist_ok = True)
+        with open(settingsPath, "w") as settingsFile:
             json.dump(settings, settingsFile)
+    else:
+        if not os.path.isfile(settingsPath):
+            print("settings.json file not found. Please set it using --settings flag")
+            sys.exit()
+        else:
+            with open(settingsPath, 'r') as settingsFile:
+                settings = json.load(settingsFile)
 
+    dirpath = settings["save_loc"]
     if args.edit:
         path = os.path.join(dirpath, args.edit + '.pkl')
         if not os.path.isfile(path):
             newList = todoList(args.edit, dirpath)
             newList.runner()
         else:
-            with open(path, 'rb') as input:
-                oldList = pickle.load(input)
+            with open(path, 'rb') as inputList:
+                oldList = pickle.load(inputList)
                 oldList.runner()
 
     elif args.remove:
@@ -48,13 +60,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--edit")
-    parser.add_argument("--remove")
-    parser.add_argument("--settings")
-
-    args = parser.parse_args()
-    main(args)
+    main()
 
 
 
